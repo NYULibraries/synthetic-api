c46151e36ffbbac853666174e57494bc030a2f91
laurxnemeth <lauranemeth.pr@gmail.com>
2020-10-13 10:21:18 -0400

    Continued setup

---
+++ spec/app.spec.js
@@ -0,0 +1,7 @@
+/* eslint-env node, jest */
+
+describe('POST api request', () => {
+    test.todo('gets called with information from synthetic tests');
+    test.todo('should post to a memcached');
+    test.todo('should contain app tested + failure type');
+  });
