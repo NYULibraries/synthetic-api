61751cda0267ba8f2066f71623059111161a7858
laurxnemeth <lauranemeth.pr@gmail.com>
2020-10-14 17:48:37 -0400

    tidy up app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const Koa = require('koa');
+const cors = require('@koa/cors')
 const Router = require('koa-router');
-const logger = require('koa-logger');
-const app = new Koa();
+const server = new Koa();
 const router = new Router();
 
-app.use(logger());
-
-router.post('/', (ctx, next) => {
- ctx.body = 'Hello World!';
+router.get('/', ctx => {
+  ctx.body = "Hello World!";
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen(3000);
+server
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(cors())
+  .listen(3003);
